b032fe20ecb861190b8ff84b8e7060293f20a42a
devpramod <pramod.pai@intel.com>
2024-08-28 23:46:07 +0000

    layer fusion for linear+gelu

---
+++ moondream_ipex/eval/docvqa.py
@@ -0,0 +1,47 @@
+import editdistance
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from datasets import load_dataset
+from tqdm import tqdm
+from .. import detect_device
+
+MODEL_ID = "vikhyatk/moondream2"
+DEVICE, DTYPE = detect_device()
+
+tokenizer = AutoTokenizer.from_pretrained(
+    MODEL_ID, trust_remote_code=True
+)
+moondream = AutoModelForCausalLM.from_pretrained(
+    MODEL_ID,
+    trust_remote_code=True,
+    attn_implementation="flash_attention_2",
+    torch_dtype=DTYPE,
+    device_map={"": DEVICE},
+)
+moondream.eval()
+
+
+def get_anls(s1, s2):
+    s1 = s1.lower().strip()
+    s2 = s2.lower().strip()
+    iou = 1 - editdistance.eval(s1, s2) / max(len(s1), len(s2))
+    anls = iou if iou >= 0.5 else 0.0
+    return anls
+
+
+docvqa_val = load_dataset("vikhyatk/docvqa", split="validation")
+
+scores = []
+for row in tqdm(docvqa_val):
+    image = row["image"]
+    enc_image = moondream.encode_image(image)
+    for qa in row["qa"]:
+        question = qa["question"]
+        answers = qa["answers"]
+        prompt = f"{question} Answer briefly."
+
+        model_answer = moondream.answer_question(enc_image, prompt, tokenizer)
+        anls = max(get_anls(model_answer, gt) for gt in answers)
+        scores.append(anls)
+
+print(MODEL_ID, REVISION)
+print("ANLS:", sum(scores) / len(scores))
---
+++ moondream_ipex/eval/pope.py
@@ -0,0 +1,66 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from datasets import load_dataset
+from tqdm import tqdm
+from .. import detect_device
+
+MODEL_ID = "vikhyatk/moondream2"
+DEVICE, DTYPE = detect_device()
+
+tokenizer = AutoTokenizer.from_pretrained(
+    MODEL_ID, trust_remote_code=True
+)
+moondream = AutoModelForCausalLM.from_pretrained(
+    MODEL_ID,
+    trust_remote_code=True,
+    attn_implementation="flash_attention_2",
+    torch_dtype=DTYPE,
+    device_map={"": DEVICE},
+)
+moondream.eval()
+
+pope_dataset = load_dataset("vikhyatk/POPE", split="test")
+
+stats = {
+    "random": (0, 0),
+    "popular": (0, 0),
+    "adversarial": (0, 0),
+}
+for row in tqdm(pope_dataset):
+    image = row["image"]
+    enc_image = moondream.encode_image(image)
+    for split in ["adversarial", "popular", "random"]:
+        for qa in row[split]:
+            question = qa["question"]
+            answer = qa["answer"]
+            prompt = f"{question}\nAnswer yes or no."
+            model_answer = moondream.answer_question(enc_image, prompt, tokenizer)
+            if model_answer.lower() == answer.lower():
+                stats[split] = (stats[split][0] + 1, stats[split][1] + 1)
+            else:
+                stats[split] = (stats[split][0], stats[split][1] + 1)
+
+print(MODEL_ID, REVISION)
+print(
+    "Random:",
+    stats["random"][0],
+    "/",
+    stats["random"][1],
+    ":",
+    stats["random"][0] * 100.0 / stats["random"][1],
+)
+print(
+    "Popular:",
+    stats["popular"][0],
+    "/",
+    stats["popular"][1],
+    ":",
+    stats["popular"][0] * 100.0 / stats["popular"][1],
+)
+print(
+    "Adversarial:",
+    stats["adversarial"][0],
+    "/",
+    stats["adversarial"][1],
+    ":",
+    stats["adversarial"][0] * 100.0 / stats["adversarial"][1],
+)
